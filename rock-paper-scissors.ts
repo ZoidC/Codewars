9b3d9a117bdcfbf95d327cdb340480c192e77b51
Bertrand Dancot <b.dancot@gmail.com>
2023-03-09 15:30:04 +0100

    Remove useless comment [dev]

--- rock-paper-scissors.ts
+++ rock-paper-scissors.ts
@@ -1,5 +1,7 @@
 // https://www.codewars.com/kata/5672a98bdbdd995fad00000f
 
+import { squareIt } from "./helpers/console";
+
 function playRockPaperScissors(input1: string, input2: string) {
     const values = ["rock", "paper", "scissors"];
     const player1 = values.indexOf(input1);
@@ -7,28 +9,15 @@ function playRockPaperScissors(input1: string, input2: string) {
     return player1 === player2 ? "Draw!" : ((player1 + values.length - 1) % values.length === player2 ? "Player 1 won!" : "Player 2 won!");
 }
 
-//  rock    paper   scissors
-//  0       1       2
-
-// win
-// (rock + 2)%2 = scissors
-// (paper + 2)%3 = rock
-// (scissors + 2)%3 = paper
-
-// draw
-// +0
-
-// lose
-// +1
-
+squareIt("Rock");
 console.log(playRockPaperScissors("rock", "rock"));
 console.log(playRockPaperScissors("rock", "paper"));
 console.log(playRockPaperScissors("rock", "scissors"));
-
+squareIt("Paper");
 console.log(playRockPaperScissors("paper", "rock"));
 console.log(playRockPaperScissors("paper", "paper"));
 console.log(playRockPaperScissors("paper", "scissors"));
-
+squareIt("Scissors");
 console.log(playRockPaperScissors("scissors", "rock"));
 console.log(playRockPaperScissors("scissors", "paper"));
 console.log(playRockPaperScissors("scissors", "scissors"));
